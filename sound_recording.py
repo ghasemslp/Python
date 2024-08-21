8fae4c4813fddfc19e1a95ea8acf50e817688e98
ghasem_slp <159786102+ghasemslp@users.noreply.github.com>
2024-08-22 00:28:21 +0330

    sound_recording.py

--- sound_recording.py
+++ sound_recording.py
@@ -13,15 +13,20 @@ seconds = 7
 
 
 #شروع ضبط
-print(Fore.YELLOW +"شروع ظبط")
+print(Fore.YELLOW +"شروع ضبط")
 myrecording = sd.rec(int(seconds * fs),samplerate=fs,channels=2,dtype='int16')
 sd.wait()
 
 #پایان ضبط
-print("پایان ظبط")
+print("پایان ضبط")
 write('ghasemslp.mkv',fs,myrecording)
 pygame.mixer.music.load('ghasemslp.mkv')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     pass
 pygame.quit()
+
+
+#لطفا از تولید کننده اصلی این کد حمایت کنید
+# id : Ghasem_slp
+# github : ghasemslp
